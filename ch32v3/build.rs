cd1e323751b4ae7e28fcab2e59e926ede33c29f0
Andelf <andelf@gmail.com>
2022-11-13 23:59:49 +0800

    chore: generate 0.1.3 crates

--- ch32v3/build.rs
+++ ch32v3/build.rs
@@ -1,16 +1,15 @@
 use std::env;
-use std::fs::File;
-use std::io::Write;
+use std::fs;
 use std::path::PathBuf;
 fn main() {
     if env::var_os("CARGO_FEATURE_RT").is_some() {
         let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-        File::create(out.join("device.x"))
-            .unwrap()
-            .write_all(include_bytes!("device.x"))
-            .unwrap();
         println!("cargo:rustc-link-search={}", out.display());
-        println!("cargo:rerun-if-changed=device.x");
+        let device_file = if env::var_os("CARGO_FEATURE_CH32V30X").is_some() {
+            "src/ch32v30x/device.x"
+        } else { panic!("No device features selected"); };
+        fs::copy(device_file, out.join("device.x")).unwrap();
+        println!("cargo:rerun-if-changed={}", device_file);
     }
     println!("cargo:rerun-if-changed=build.rs");
 }
