38e78e725fac664a8b705a882349c651aa2e782c
Andelf <andelf@gmail.com>
2022-11-14 00:45:04 +0800

    chore: generate 0.1.4 crates

---
+++ ch58x/src/lib.rs
@@ -0,0 +1,25 @@
+//! Peripheral access API for CH58X microcontrollers
+//! (generated using [svd2rust](https://github.com/rust-embedded/svd2rust)
+//! 0.26.0)
+//!
+//! You can find an overview of the API here:
+//! [svd2rust/#peripheral-api](https://docs.rs/svd2rust/0.26.0/svd2rust/#peripheral-api)
+//!
+//! For more details see the README here:
+//! [ch32-rs](https://github.com/ch32-rs/ch32-rs)
+//!
+//! This crate supports all CH58X devices; for the complete list please
+//! see:
+//! [ch58x](https://crates.io/crates/ch58x)
+//!
+
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![no_std]
+
+mod generic;
+pub use self::generic::*;
+
+#[cfg(feature = "ch58x")]
+pub mod ch58x;
+
