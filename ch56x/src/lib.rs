57b411832199caaed12c82d376468af3a24e135f
Andelf <andelf@gmail.com>
2022-12-05 23:41:07 +0800

    feat: add ch56x

---
+++ ch56x/src/lib.rs
@@ -0,0 +1,25 @@
+//! Peripheral access API for CH56X microcontrollers
+//! (generated using [svd2rust](https://github.com/rust-embedded/svd2rust)
+//! 0.26.0)
+//!
+//! You can find an overview of the API here:
+//! [svd2rust/#peripheral-api](https://docs.rs/svd2rust/0.26.0/svd2rust/#peripheral-api)
+//!
+//! For more details see the README here:
+//! [ch32-rs](https://github.com/ch32-rs/ch32-rs)
+//!
+//! This crate supports all CH56X devices; for the complete list please
+//! see:
+//! [ch56x](https://crates.io/crates/ch56x)
+//!
+
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![no_std]
+
+mod generic;
+pub use self::generic::*;
+
+#[cfg(feature = "ch56x")]
+pub mod ch56x;
+
